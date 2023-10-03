1ad0b074291cc18b011ba61845210b1bf93d936d
Samuel Ješík <119365050+SamuelJesik@users.noreply.github.com>
2023-10-03 21:08:44 +0200

    Revert "Add files via upload"

--- hanoi.c
+++ hanoi.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void hanoi(int n, int a, int b,in)
+void hanoi(int n, int a, int b)
 {
   if (n == 1) 
     printf("move stone 1 from %d to %d\n", a, b);
@@ -8,7 +8,7 @@ void hanoi(int n, int a, int b,in)
   {
     hanoi(n - 1, a, 6 - a);
     printf("move stone %d from %d to %d\n", n, a, b);
-    hanoi(n-1, 6 - a - b, b);
+    hanoi(n, 6 - a - b, b);
   }
 }
 
