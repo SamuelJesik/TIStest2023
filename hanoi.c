053850b9eb6099590bd6298be07026a633066907
Re3der <62442557+Re3der@users.noreply.github.com>
2023-10-09 14:02:04 +0200

    Fix hanoi.c
    Fixed kod

--- hanoi.c
+++ hanoi.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-void hanoi(int n, int a, int b,in)
+void hanoi(int n, int a, int b,int c)
 {
-  if (n == 1) 
-    printf("presun kamen 1 z %d do %d\n", a, b);
-  else
-  {
-    hanoi(n - 1, a, 3 - a);
-    printf("presun kamen %d z %d do %d\n", n, a, b);
-    hanoi(n-1 , 3 - a - b, b);
-  }
+    if (n == 1)
+        printf("presun kamen 1 z %d do %d\n", a, b);
+    else
+    {
+        hanoi(n - 1, a,c,b);
+        printf("presun kamen %d z %d do %d\n", n,a,b);
+        hanoi(n - 1 ,c,b,a);
+    }
 }
 
 int main()
 {
-  int n = 4;
+    int n = 3;
 
-  hanoi(n, 1, 2);
-  return 0;
+    hanoi(n, 1, 3,2);
+    return 0;
 }
+
